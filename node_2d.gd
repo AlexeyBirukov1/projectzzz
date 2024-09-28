30662582b91361f587d6527f9cb4d0772fec2f78
user.email <my_name@example.com>
2024-09-28 17:37:43 +0700

    добавил код для игрока, удалил привет txt

--- node_2d.gd
+++ node_2d.gd
@@ -1,11 +1,30 @@
-extends Node2D
+extends CharacterBody2D
 
+# Скорость перемещения персонажа
+var speed = 200
+# Сила прыжка
+var jump_force = -400
+# Гравитация
+var gravity = 800
 
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
+# Вектор движения
+var vel = Vector2()
 
+func _process(delta):
+	# Применяем гравитацию
+	velocity.y += gravity * delta
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
+	# Обрабатываем ввод с клавиатуры
+	if Input.is_action_pressed("ui_right"):
+		velocity.x = speed
+	elif Input.is_action_pressed("ui_left"):
+		velocity.x = -speed
+	else:
+		velocity.x = 0
+
+	# Прыжок
+	if is_on_floor() and Input.is_action_just_pressed("ui_select"):
+		velocity.y = jump_force
+
+	# Перемещаем персонажа с помощью KinematicBody2D
+	vel = move_and_slide(velocity, Vector2.UP)
